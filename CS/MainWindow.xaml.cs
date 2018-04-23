2e7819f54e973f54903745195f191186d5c97d18
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows;
+
+using DevExpress.XtraRichEdit;
+
+namespace DocumentServer_PrintToPDF {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        PerformanceCounter counter;
+        bool executing;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            tbPath.Text = Application.Current.StartupUri.AbsolutePath;
+
+            string procName = Process.GetCurrentProcess().ProcessName;
+            counter = new PerformanceCounter("Process", "Working Set - Private", procName);
+            ShowMemoryUsage();
+        }
+
+        string PrintToPDF(RichEditDocumentServer server, string filePath) {
+            try {
+                server.LoadDocument(filePath);
+            }
+            catch (Exception ex) {
+                server.CreateNewDocument();
+                return String.Format("{0:T} Error:{1} -> {2}", DateTime.Now, ex.Message, filePath) + Environment.NewLine;
+            }
+            string outFileName = Path.ChangeExtension(filePath, "pdf");
+            FileStream fsOut = File.Open(outFileName, FileMode.Create);
+            server.ExportToPdf(fsOut);
+            fsOut.Close();
+            return String.Format("{0:T} Done-> {1}", DateTime.Now, outFileName) + Environment.NewLine;
+        }
+
+        void ConvertFiles(string path) {
+            if (!Directory.Exists(path))
+                return;
+
+            string[] files = System.IO.Directory.GetFiles(path, "*.doc?", System.IO.SearchOption.AllDirectories);
+            InitProgress(files.Length);
+            using (RichEditDocumentServer server = new RichEditDocumentServer()) {
+                int count = files.Length;
+                for (int i = 0; i < count; i++) {
+                    string progress = PrintToPDF(server, files[i]);
+                    AppendProgress(progress, i + 1);
+                    if (!executing)
+                        return;
+                }
+            }
+        }
+
+        void btnConvert_Click(object sender, RoutedEventArgs e) {
+            if (!executing)
+                StartConversion();
+            else
+                FinishConversion();
+        }
+
+        void StartConversion() {
+            executing = true;
+            btnConvert.Content = "Stop!";
+            tbPath.IsReadOnly = true;
+            pnlProgress.Visibility = System.Windows.Visibility.Visible;
+            Thread worker = new Thread(BackgroundWorker);
+            worker.Start(tbPath.Text);
+        }
+        void FinishConversion() {
+            pnlProgress.Visibility = System.Windows.Visibility.Collapsed;
+            executing = false;
+            tbPath.IsReadOnly = false;
+            btnConvert.Content = "Start!";
+        }
+        void BackgroundWorker(object parameter) {
+            string path = parameter as string;
+            if (String.IsNullOrEmpty(path))
+                return;
+            ConvertFiles(path);
+            ShowMemoryUsage();
+
+            Action action = delegate() { FinishConversion(); };
+            Dispatcher.BeginInvoke(action);
+        }
+
+        void InitProgress(int fileCount) {
+            Action action = delegate() {
+                edtProgress.Minimum = 0;
+                edtProgress.Maximum = fileCount;
+                edtProgress.EditValue = 0;
+            };
+            this.Dispatcher.Invoke(action);
+        }
+        void AppendProgress(string displayText, int fileIndex) {
+            Action action = delegate() {
+                tbLog.Text += displayText;
+                LogScrollViewer.ScrollToVerticalOffset(LogScrollViewer.Height);
+                edtProgress.EditValue = fileIndex;
+            };
+            this.Dispatcher.Invoke(action);
+        }
+        void ShowMemoryUsage() {
+            Action action = delegate() { lblMemoryUsage.Text = String.Format("Memory usage: {0:N0} K", counter.RawValue / 1024); };
+            this.Dispatcher.Invoke(action);
+        }
+        void Window_Closed(object sender, EventArgs e) {
+            executing = false;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,15 @@
+# DXRichEdit for WPF: How to implement automatic Document to PDF converter
+
+
+<p>This example demonstrates how easy it is to implement automatic conversion from <strong>.DOCX</strong> and <strong>.DOC</strong> files to <strong>PDF </strong>format using the <strong>RichEditDocumentServer</strong>. <br />
+RichEditDocumentServer determines format of a file specified in the <strong>Load </strong>method by its extension. After a document is loaded, the <strong>ExportToPdf </strong>method is used to produce a PDF stream.</p><p><br />
+<img src="https://raw.githubusercontent.com/DevExpress-Examples/dxrichedit-for-wpf-how-to-implement-automatic-document-to-pdf-converter-e3295/13.1.6+/media/34cce332-1b05-4936-bc05-ff40cf0d2e94.png"></p>
+
+
+<h3>Description</h3>
+
+<p>Starting from v2013 vol. 1.6 the use of the RichEditDocumentServer requires the reference to the DevExpress.Docs.vXY.z.dll assembly and a valid license for the Document Server.</p>
+
+<br/>
+
+
